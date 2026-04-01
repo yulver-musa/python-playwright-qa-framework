3066b0ce93dae4cf715cb4c6cb395b3170879ef1
yulver-musa <yulver.musa@gmail.com>
2026-04-01 14:31:51 +0100

    Test cases with markers

--- conftest.py
+++ conftest.py
@@ -1,5 +1,13 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from pages.login_page import LoginPage
+
+
+@pytest.fixture
+def login_page(page):
+    login = LoginPage(page)
+    login.open()
+    return login
 
 
 @pytest.fixture(scope="session")
--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,11 +1,10 @@
 import pytest
-from pages.login_page import LoginPage
 from data.login_cases import LOGIN_TEST_CASES
 
+@pytest.mark.smoke
+@pytest.mark.regression
 @pytest.mark.parametrize("case", LOGIN_TEST_CASES)
-def test_login(page, case):
-    login_page = LoginPage(page)
-    login_page.open()
+def test_login(login_page, case):
     login_page.login(case["username"], case["password"])
 
     assert case["expected_message"] in login_page.get_flash_message()
