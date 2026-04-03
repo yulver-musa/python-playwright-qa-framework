57979d1d777f13911162576f2bec5813556301b3
yulver-musa <yulver.musa@gmail.com>
2026-04-03 09:30:53 +0100

    Deleted the example test.

--- tests/ui/test_example.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import Page
-
-def test_homepage_loads(page: Page):
-    page.goto("https://www.businesspromanagement.co.za/")
-    assert "Free Accounting Software by Zoho - BPM" in page.title()
