3584c6ae43d6576343bb1de648feffe1cf12a825
yulver-musa <yulver.musa@gmail.com>
2026-04-13 19:20:13 +0100

    Updated the performance test

--- performance/test_posts.js
+++ performance/test_posts.js
@@ -1,5 +1,10 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 10,          // 10 virtual users
+  duration: '10s',  // run for 10 seconds
+};
 
 export default function () {
   const res = http.get('https://jsonplaceholder.typicode.com/posts');
@@ -7,4 +12,6 @@ export default function () {
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
+
+  sleep(1);
 }
